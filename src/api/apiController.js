0452c7146ffad61329e51282962e8f783439cfbc
Kenan Tomfie Bukit <119487201+khensin166@users.noreply.github.com>
2025-06-25 00:06:03 +0700

    hasan api

--- src/api/apiController.js
+++ src/api/apiController.js
@@ -3,6 +3,6 @@ const API_URL1 = "https://web-production-9d46e.up.railway.app";
 const API_URL2 = "https://web-production-09fef.up.railway.app";
 // const API_URL2 = "http://localhost:8000";
 const API_URL3 = "https://web-production-60d6.up.railway.app/api";
-const API_URL4 = "https://backendta-production-8608.up.railway.app";
+const API_URL4 = "https://backendta-production-17e4.up.railway.app";
 
 export { API_URL1, API_URL2, API_URL3, API_URL4 };
